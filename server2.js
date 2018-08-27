0c51aa1ebe6789d64728dde05231862b48992660
gradybknight <grady.knight@gmail.com>
2018-08-27 11:53:38 -0400

    rewrite server to have control system variable at server level

--- server2.js
+++ server2.js
@@ -1,27 +1,27 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+// ***********************************************   Package Imports   ***********************************************
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const router = express.Router();
+const phidget22 = require('phidget22');
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3001;
+// ***********************************************   Module Imports   ***********************************************
+const fractionalStill = require('./secondTry');
 
-// Sets up the Express app to handle data parsing
+// ***********************************************   Express Server Setup   ***********************************************
+const PORT = 3001;
+const app = express();
+
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(router);
 
-
-// Phidget Information
-const startFractionalRun = require('./secondTry');
-
-// pot still variables
+// ***********************************************   Pot Still Variables   *******************************************
 let serverPotStatus = false;
 let serverGraphData = [];
 
-// fractional still variables
+// ***********************************************   Fractional Still Variables   ************************************
 let serverFractionalStatus = false;
 let fractionalGraphData = [];
 let serverRunOverview = {
@@ -33,53 +33,139 @@ let serverRunOverview = {
   startAlcohol: 0,
   startVolume: 0,
   currentMessage:'not running'
+};
+let fractionalControlSystem = {};
+
+// ***********************************************   Phidget Board Initialization ************************************
+console.log('Phidget connecting');
+var SERVER_PORT = 5661;
+var hostName = '127.0.0.1';
+var conn = new phidget22.Connection(SERVER_PORT, hostName, { name: 'Server Connection', passwd: '' });
+conn.connect()
+  .then(initializePhidgetBoards(fractionalControlSystem))
+  .catch(function (err) {
+    console.error('Error connecting to phidget:', err.message);
+    process.exit(1);
+  });
+
+async function initializePhidgetBoards(fractionalControlSystem) {
+  let heatingElement = new phidget22.DigitalOutput();
+  heatingElement.setChannel(0);
+  await heatingElement.open();
+  console.log('heating element attached');
+
+  let solenoid = new phidget22.DigitalOutput();
+  solenoid.setChannel(1);
+  await solenoid.open();
+  console.log('solenoid attached');
+
+  let extendArm = new phidget22.DigitalOutput();
+  extendArm.setChannel(2);
+  await extendArm.open();
+  console.log('arm extender attached');
+
+  let retractArm = new phidget22.DigitalOutput();
+  retractArm.setChannel(3);
+  await retractArm.open();
+  console.log('arm retractor attached');
+
+  var tempProbe = new phidget22.TemperatureSensor();
+  tempProbe.setChannel(0);
+  tempProbe.setDataInterval(500);
+  await tempProbe.open();
+  console.log('temp probe attached');
+
+  fractionalControlSystem = {
+      heatingElement:heatingElement,
+      solenoid:solenoid,
+      extendArm:extendArm,
+      retractArm:retractArm,
+      tempProbe:tempProbe
+  };
+  console.log(`Fractional still control system established`);
+  return true;
 }
 
+// ***********************************************   Routes   ********************************************************
+// ***********************************************   Pot Still Routes   **********************************************
+router.route('/setpot')
+  .post((req,res) => {
+    serverPotStatus = req.body.desiredPotState
+    flashingLights.runDemo();
+    res.json({
+      serverPotStatus:serverPotStatus
+    });
+  })
 
-// Routes
-// =============================================================
+router.route('/potstatus')
+  .get((req,res) => {
+    console.log('front end asked what is the pot status')
+    console.log(`server status is ${serverPotStatus}`);
+    res.json({
+      serverPotStatus:serverPotStatus
+    });
+  })
 
-app.get("*", function(req, res){
-    console.log('got this')
-})
+router.route('/potgraphdata')
+  .get((req,res) => {
+    console.log('front end asked for graph data')
+    res.json({
+      serverGraphData:serverGraphData
+    });
+  })
 
-// FRACTIONAL STILL
-app.post("/setfractional", function(req,res) {
+// ***********************************************   Fractional Still Routes   ****************************************
+router.route('/setfractional')
+  .post((req,res) => {
     serverRunOverview.startAlcohol=parseInt(req.body.startAlcohol);
     serverRunOverview.startVolume=parseInt(req.body.startVolume);
     serverFractionalStatus=req.body.desiredFractionalState;
     fractionalGraphData=[];
-    startFractionalRun(fractionalGraphData, serverFractionalStatus, serverRunOverview);
+    console.log('starting frac');
+    fractionalStill.startFractionalRun(fractionalGraphData, serverFractionalStatus, serverRunOverview);
     res.json({
       serverFractionalStatus:serverFractionalStatus
     })
-});
+  })
 
-app.get("fractionalstatus", function(req,res) {
-    console.log('front end asked what is the fractional status')
+router.route('/fractionalstatus')
+  .get((req,res) => {
+    console.log('front end asked what is the pot status')
     console.log(`server status is ${serverFractionalStatus}`);
     res.json({
       serverFractionalStatus:serverFractionalStatus
     });
-});
+  })
 
-app.get("/fractionalgraphdata", function(req,res) {
-    console.log('front end asked for fractional graph data')
+router.route('/fractionalgraphdata')
+  .get((req,res) => {
+    console.log('front end asked for graph data')
     res.json({
       fractionalGraphData:fractionalGraphData
     });
-});
+  })
 
-app.get("/fractionalsummary", function(req,res) {
+router.route('/fractionalsummary')
+  .get((req,res) => {
     console.log('front end asked for fractional summary')
     res.json({
       serverRunOverview:serverRunOverview
     });
-});
+  })
+
+  router.route('/checkfractionaltemperature')
+  .get((req,res) => {
+    console.log('front end asked for fractional temperature');
+    let fractionalTemp = fractionalControlSystem.tempProbe.getTemperature();
+    res.json({
+      fractionalTemp:fractionalTemp
+    });
+  })
 
 
-// Starts the server to begin listening
-// =============================================================
+// ***********************************************   Start API server   ****************************************
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+
