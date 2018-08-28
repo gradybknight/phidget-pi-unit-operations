7ef8c9d2159b9d2ec09b59fc4207dc510a0f7426
gradybknight <grady.knight@gmail.com>
2018-08-27 22:38:36 -0400

    local globals

--- simplifiedProgram.js
+++ simplifiedProgram.js
@@ -2,76 +2,75 @@ function startSimplifiedProgram(fractionalGraphData, serverFractionalStatus, ser
     let overallArray = [];
     let startTime = Date.now();
 
-    let fractionalGraphData = fractionalGraphData;
-    let serverFractionalStatus = serverFractionalStatus;
-    let serverRunOverview = serverRunOverview;
-    let fractionalControlSystem = fractionalControlSystem;
+    let fractionalGraphDataLocal = fractionalGraphData;
+    let serverRunOverviewLocal = serverRunOverview;
+    let fractionalControlSystemLocal = fractionalControlSystem;
 
-    console.log(fractionalControlSystem);
+    console.log(fractionalControlSystemLocal);
 
 
-    function buildOuterArray(serverRunOverview) {
-        let numberOfCycles = Math.floor(serverRunOverview.startAlcohol * serverRunOverview.startVolume);
+    function buildOuterArray() {
+        let numberOfCycles = Math.floor(serverRunOverviewLocal.startAlcohol * serverRunOverviewLocal.startVolume);
         for (let i=0; i<numberOfCycles; i++) {
             let individualCycle = {};
             individualCycle.closeTime = .5 * (i+1);
             overallArray.push(individualCycle);
         };
     };
-    function processOverallArray(fractionalControlSystem, serverRunOverview, overallArray){
-        console.log(serverRunOverview);
+    function processOverallArray(){
+        console.log(serverRunOverviewLocal);
         let counter = 0;
         function runOneCycle() {
-            fractionalControlSystem.solenoid.setState(true);
+            fractionalControlSystemLocal.solenoid.setState(true);
             console.log('opened valve');
             setTimeout(endOpenValve, 500);
         };
         
         function endOpenValve() {
-            fractionalControlSystem.solenoid.setState(false);
+            fractionalControlSystemLocal.solenoid.setState(false);
             console.log('closed valve');
             setTimeout(waitUntilNextCycle, 4000);
         };
         function waitUntilNextCycle() {
             counter++;
-            serverRunOverview.currentClickCountInBeaker=counter;
+            serverRunOverviewLocal.currentClickCountInBeaker=counter;
             
             if (counter < overallArray.length) {
                 runOneCycle();
             } else {
                 console.log('testing that i can see overall array:');
                 console.log(overallArray);
-                fractionalControlSystem.solenoid.setState(false);
-                fractionalControlSystem.heatingElement.setState(false);
+                fractionalControlSystemLocal.solenoid.setState(false);
+                fractionalControlSystemLocal.heatingElement.setState(false);
                 console.log(`Finished Run at ${Date.now()}`);
                 clearInterval(tempLoggingTimer);
                 console.log(`temp logging stopped`);
-                console.log(fractionalGraphData);
+                console.log(fractionalGraphDataLocal);
             }
         };
         runOneCycle();
     };
-    function waitAfterPreHeat(fractionalControlSystem, serverRunOverview, overallArray, fractionalGraphData) {
+    function waitAfterPreHeat() {
         let waitTime = .25 * 60 * 1000;
-        setTimeout( () => processOverallArray(fractionalControlSystem, serverRunOverview, overallArray, fractionalGraphData), waitTime)  
+        setTimeout( () => processOverallArray(), waitTime)  
     };
-    function logTemperature(fractionalControlSystem, fractionalGraphData, startTime) {
-        let fractionalTemp = fractionalControlSystem.tempProbe.getTemperature();
+    function logTemperature(startTime) {
+        let fractionalTemp = fractionalControlSystemLocal.tempProbe.getTemperature();
         let dataPoint = {}
         dataPoint.y = fractionalTemp;
         dataPoint.x = Date.now() - startTime;
         dataPoint.id = Date.now();
-        fractionalGraphData.push(dataPoint);
+        fractionalGraphDataLocal.push(dataPoint);
         console.log(`Pushed temperature: ${fractionalTemp} to array at ${Date.now()}`);
     }
-    let tempLoggingTimer = setInterval( () => logTemperature(fractionalControlSystem, fractionalGraphData, startTime), 5000);
+    let tempLoggingTimer = setInterval( () => logTemperature(startTime), 5000);
     console.log(`starting run now: ${Date.now()}`);
-    buildOuterArray(serverRunOverview);
+    buildOuterArray(serverRunOverviewLocal);
     console.log(`Outer Array: `);
     console.log(overallArray);
     fractionalControlSystem.heatingElement.setState(true);
     console.log('heating element is on');
-    waitAfterPreHeat(fractionalControlSystem, serverFractionalStatus, overallArray, fractionalGraphData);
+    waitAfterPreHeat();
 
 }
 
