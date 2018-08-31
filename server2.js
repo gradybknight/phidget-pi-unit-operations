b111725508035d71db96f2550e88e75ba839d3d3
gradybknight <grady.knight@gmail.com>
2018-08-30 20:17:05 -0400

    CORS fix

--- server2.js
+++ server2.js
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(router);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // ***********************************************   Pot Still Variables   *******************************************
 let serverPotStatus = false;
 let serverGraphData = [];
