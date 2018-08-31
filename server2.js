10b62f530ff48867e4117c790a2560f2a7314e6a
gradybknight <grady.knight@gmail.com>
2018-08-30 20:39:48 -0400

    test CORS fix

--- server2.js
+++ server2.js
@@ -13,9 +13,25 @@ const fractionalStillSingleInteraction = require('./fractionalstillinteractions'
 const PORT = 3001;
 const app = express();
 
+//ADDED
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(allowCrossDomain);  //ADDED
 app.use(router);
 
 app.use(function(req, res, next) {
